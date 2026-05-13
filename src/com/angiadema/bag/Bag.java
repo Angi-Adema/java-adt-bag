a314b8786b39dacb6c80871f92eaa8bc12bde86a
Angi-Adema <dr.adema@gmail.com>
2026-05-13 11:01:44 -0600

    added the Bag class with the logic to conduct the functionality of the Bag object. Added the main method.

--- src/com/angiadema/bag/Bag.java
+++ src/com/angiadema/bag/Bag.java
@@ -1,5 +1,43 @@
 package com.angiadema.bag;
 
-public class Bag {
+import java.util.ArrayList;
+
+// Implement a Bag class with generic type
+public class Bag<T> {
+	
+	// ArrayList to hold bag items
+	private ArrayList<T> items;
+	
+	// Add item of type T to the bag
+	public void add(T item) {
+		items.add(item);
+	}
+	
+	// Remove one occurrence of the item from bag
+	void remove(T item) {
+		items.remove(item);
+	}
+	
+	// Return true or false if the item exists
+	boolean contains(T item) {
+		return items.contains(item);
+	}
+	
+	// Return the number of occurrences of an item
+	int count(T item) {
+		
+		// Variable to hold the number item appears in bag
+		int totalDupItem = 0;
+		
+		// Loop through the items array to get total
+		for (T currentVal : items) {
+			
+			// Compare current value with item, add if the same
+			if (currentVal.equals(item)) {
+				totalDupItem++;
+			}
+		}
+		return totalDupItem;
+	}
 
 }
---
+++ src/com/angiadema/bag/Main.java
@@ -0,0 +1,10 @@
+package com.angiadema.bag;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
