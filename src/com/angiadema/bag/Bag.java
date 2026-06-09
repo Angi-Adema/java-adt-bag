53025094e031f101bbbfaec27f4f142d0d2b3200
Angi-Adema <dr.adema@gmail.com>
2026-06-09 10:48:07 -0600

    refactored the distinct() method to use an enhanced for loop and a set rather than .contains() to save a list of distinct items.

--- src/com/angiadema/bag/Bag.java
+++ src/com/angiadema/bag/Bag.java
@@ -1,6 +1,7 @@
 package com.angiadema.bag;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 // Implement a Bag class with generic type
 public class Bag<T> {
@@ -60,19 +61,40 @@ public class Bag<T> {
 	}
 	
 	// Method to print only the item names and not the quantities
+	// REFACTORED: Instead of using a standard for loop, an enhanced for loop is used
+	//             in conjunction with a set to return a list of unique items
 	public Bag<T> distinct() {
 		
 		// Create a new Bag object for the distinct list
 		Bag<T> distinctItems = new Bag<>();
 		
-		// Loop through items in current bag and add to new bag if not already there
-		for (int i = 0; i < items.size(); i++) {
-			if (!distinctItems.contains(items.get(i))) {
-				distinctItems.add(items.get(i));
+		// Instead of using .contains() to create a list of distinct items, use a Set
+		Set<T> existingItems = new HashSet<>();
+		
+		// Loop through items using an enhanced for loop adding distinct items to the Bag
+		// after checking the Set
+		for (T item : items) {
+			if (existingItems.add(item)) {
+				distinctItems.add(item);
 			}
 		}
 		return distinctItems;
 	}
+	
+	// COMMENTED OUT ORIGINAL CODE
+//	public Bag<T> distinct() {
+//		
+//		// Create a new Bag object for the distinct list
+//		Bag<T> distinctItems = new Bag<>();
+//		
+//		// Loop through items in current bag and add to new bag if not already there
+//		for (int i = 0; i < items.size(); i++) {
+//			if (!distinctItems.contains(items.get(i))) {
+//				distinctItems.add(items.get(i));
+//			}
+//		}
+//		return distinctItems;
+//	}
 	
 	// ArrayList is set to private, print method to print contents
 	public void printBagContents() {
